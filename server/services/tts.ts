bf1c2bb67f18f3797b57b8cf7728328746a4a085
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2026-04-10 01:50:29 +0100

    feat: Random voices

--- server/services/tts.ts
+++ server/services/tts.ts
@@ -1,8 +1,50 @@
 import { saveAudio } from '../storage/audioStore.js'
 
 const TTS_ENDPOINT = 'https://texttospeech.googleapis.com/v1/text:synthesize'
-const TTS_VOICE_NAME = 'en-GB-Neural2-B'
 const TTS_LANGUAGE_CODE = 'en-GB'
+const TTS_VOICE_NAMES = [
+  'en-GB-Chirp3-HD-Achernar',
+  'en-GB-Chirp3-HD-Achird',
+  'en-GB-Chirp3-HD-Algenib',
+  'en-GB-Chirp3-HD-Algieba',
+  'en-GB-Chirp3-HD-Alnilam',
+  'en-GB-Chirp3-HD-Aoede',
+  'en-GB-Chirp3-HD-Autonoe',
+  'en-GB-Chirp3-HD-Callirrhoe',
+  'en-GB-Chirp3-HD-Charon',
+  'en-GB-Chirp3-HD-Despina',
+  'en-GB-Chirp3-HD-Enceladus',
+  'en-GB-Chirp3-HD-Erinome',
+  'en-GB-Chirp3-HD-Fenrir',
+  'en-GB-Chirp3-HD-Gacrux',
+  'en-GB-Chirp3-HD-Iapetus',
+  'en-GB-Chirp3-HD-Kore',
+  'en-GB-Chirp3-HD-Laomedeia',
+  'en-GB-Chirp3-HD-Leda',
+  'en-GB-Chirp3-HD-Orus',
+  'en-GB-Chirp3-HD-Pulcherrima',
+  'en-GB-Chirp3-HD-Puck',
+  'en-GB-Chirp3-HD-Rasalgethi',
+  'en-GB-Chirp3-HD-Sadachbia',
+  'en-GB-Chirp3-HD-Sadaltager',
+  'en-GB-Chirp3-HD-Schedar',
+  'en-GB-Chirp3-HD-Sulafat',
+  'en-GB-Chirp3-HD-Umbriel',
+  'en-GB-Chirp3-HD-Vindemiatrix',
+  'en-GB-Chirp3-HD-Zephyr',
+  'en-GB-Chirp3-HD-Zubenelgenubi',
+  'en-GB-Neural2-A',
+  'en-GB-Neural2-B',
+  'en-GB-Neural2-C',
+  'en-GB-Neural2-D',
+  'en-GB-Neural2-F',
+  'en-GB-Neural2-N',
+  'en-GB-Neural2-O',
+] as const
+
+function pickRandomVoice(): string {
+  return TTS_VOICE_NAMES[Math.floor(Math.random() * TTS_VOICE_NAMES.length)]
+}
 
 export interface TtsSuccess {
   audioUrl: string
@@ -52,7 +94,7 @@ export async function generateAudio(word: string): Promise<TtsResult> {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         input: { text: word },
-        voice: { languageCode: TTS_LANGUAGE_CODE, name: TTS_VOICE_NAME },
+        voice: { languageCode: TTS_LANGUAGE_CODE, name: pickRandomVoice() },
         audioConfig: { audioEncoding: 'MP3' },
       }),
     })
